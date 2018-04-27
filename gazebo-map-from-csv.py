d1cceeb0bf2a945e9ec7131419bf7bf3714c19b3
jgvictores <jcgvicto@gmail.com>
2018-04-27 20:47:20 +0200

    update url

--- gazebo-map-from-csv.py
+++ gazebo-map-from-csv.py
@@ -2,7 +2,7 @@
 
 # Authors: Juan G Victores
 # CopyPolicy: released under the terms of the LGPLv2.1
-# URL: https://github.com/jgvictores/gazebo-map-tools
+# URL: https://github.com/roboticslab-uc3m/gazebo-tools
 
 from lxml import etree
 
